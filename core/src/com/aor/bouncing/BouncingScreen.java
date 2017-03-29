ae00897a0e4ab2e1795886e4e30d1a85e332f6d7
Andre Restivo <andre.restivo@gmail.com>
2017-03-29 14:46:48 +0100

    Added a camera and physical units

--- core/src/com/aor/bouncing/BouncingScreen.java
+++ core/src/com/aor/bouncing/BouncingScreen.java
@@ -1,12 +1,25 @@
 package com.aor.bouncing;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 
 /**
  * The only game screen.
  */
 public class BouncingScreen extends ScreenAdapter {
+    /**
+     * Game width in meters
+     */
+    private static final int GAME_WIDTH = 6;
+
+    /**
+     * A football is 22cm in diameter and the sprite has a width of 200px
+     */
+    private static final float PIXEL_TO_METER = 0.22f / 200;
+
     /**
      * The game.
      */
@@ -16,6 +29,8 @@ public class BouncingScreen extends ScreenAdapter {
      * The ball texture.
      */
     private final Texture ballTexture;
+
+    private final OrthographicCamera camera;
 
     /**
      * Creates a new screen loadings all needed textures.
@@ -29,6 +44,8 @@ public class BouncingScreen extends ScreenAdapter {
         game.getAssetManager().finishLoading();
 
         ballTexture = (Texture) game.getAssetManager().get("ball.png");
+
+        camera = new OrthographicCamera(GAME_WIDTH / PIXEL_TO_METER, GAME_WIDTH/ PIXEL_TO_METER * ((float) Gdx.graphics.getHeight() / (float)Gdx.graphics.getWidth()));
     }
 
     /**
@@ -40,6 +57,15 @@ public class BouncingScreen extends ScreenAdapter {
     public void render(float delta) {
         super.render(delta);
 
+        // Update the camera
+        camera.update();
+        game.getBatch().setProjectionMatrix(camera.combined);
+
+        // Clear the screen
+        Gdx.gl.glClearColor( 103/255f, 69/255f, 117/255f, 1 );
+        Gdx.gl.glClear( GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT );
+
+        // Draw the texture
         game.getBatch().begin();
         game.getBatch().draw(ballTexture, 100, 100);
         game.getBatch().end();
