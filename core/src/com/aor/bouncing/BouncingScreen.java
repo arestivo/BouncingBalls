9fa3fa93095bd3380ab189a8d5a54bed4e1bda4f
Andre Restivo <andre.restivo@gmail.com>
2017-03-29 17:16:03 +0100

    Clear the screen

--- core/src/com/aor/bouncing/BouncingScreen.java
+++ core/src/com/aor/bouncing/BouncingScreen.java
@@ -1,6 +1,8 @@
 package com.aor.bouncing;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 
 /**
@@ -40,6 +42,11 @@ public class BouncingScreen extends ScreenAdapter {
     public void render(float delta) {
         super.render(delta);
 
+        // Clear the screen
+        Gdx.gl.glClearColor( 103/255f, 69/255f, 117/255f, 1 );
+        Gdx.gl.glClear( GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT );
+
+        // Draw the texture
         game.getBatch().begin();
         game.getBatch().draw(ballTexture, 100, 100);
         game.getBatch().end();
